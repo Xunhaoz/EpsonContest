cf84892b29810629a452a525ff0d86307c7a7d58
xunhaoz <leo20020529@gmail.com>
2025-04-18 18:23:07 +0800

    update index page

--- components/index_component/IndexComponent.py
+++ components/index_component/IndexComponent.py
@@ -2,23 +2,33 @@ import gradio as gr
 
 
 class IndexComponent:
+    head = """
+    <style>
+    .gallery_cover {
+        border-radius: 20px !important;
+    }
+    </style>
+    """
+
     def __init__(self):
-        with gr.Blocks(fill_height=True) as self.index_page:
-            gr.Markdown("# Epson Innovation Challenge 2025 創新大賽")
-            gr.Image()
+        with gr.Blocks(fill_height=True, head=self.head) as self.index_page:
+            gr.Image('static/index.png', container=False, show_label=False, show_download_button=False, show_fullscreen_button=False)
 
             with gr.Row():
                 with gr.Column():
-                    gr.Markdown("# 填色線稿畫廊")
-                    gr.Image()
+                    gr.Image(
+                        'static/cut_paste_cover.jpg', elem_classes="gallery_cover",
+                        container=False, show_label=False, show_download_button=False, show_fullscreen_button=False)
                     gr.Button("查看更多!!", link="/coloring_gallery")
                 with gr.Column():
-                    gr.Markdown("# 拼貼畫廊")
-                    gr.Image()
+                    gr.Image(
+                        'static/coloring_cover.jpg', elem_classes="gallery_cover",
+                        container=False, show_label=False, show_download_button=False, show_fullscreen_button=False)
                     gr.Button("查看更多!!", link="/collage_gallery")
                 with gr.Column():
-                    gr.Markdown("# 巨海製作器")
-                    gr.Image(height="100%")
+                    gr.Image(
+                        'static/worksheet_cover.jpg', elem_classes="gallery_cover",
+                        container=False, show_label=False, show_download_button=False, show_fullscreen_button=False)
                     gr.Button("查看更多!!", link="/huge_poster_creation")
 
             gr.Button("體驗生成式人工智慧!!", link="/generative_ai")
